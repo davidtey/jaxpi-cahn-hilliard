9219491ac3cd3637fc0b9834425d0bfe4409de95
davidtey <davidteyby@gmail.com>
2025-02-16 14:23:45 +0800

    new equation

--- examples/cahn_hilliard_extended/data/gen_ch_extended.m
+++ examples/cahn_hilliard_extended/data/gen_ch_extended.m
@@ -4,7 +4,7 @@ steps = 500;
 N = 511;
 xmin = -1;
 xmax = 1;
-tmax = 2;
+tmax = 4;
 
 p = 0.5;
 a = 10;
@@ -16,8 +16,8 @@ u0 = chebfun('0.2*(sin(10*pi*x))^3 - 0.8*sin(9*pi*x)', dom, 'trig');
 % u_t = 1e-2*(-u_xx - 1e-3*u_xxxx + (u^3)_xx - u)
 
 S = spinop(dom, tspan);
-S.lin = @(u) -0.25*diff(u, 2) - 0.00025*diff(u, 4) - 0.05*u;
-S.nonlin = @(u) 0.25*diff(u.^3, 2);
+S.lin = @(u) -0.1*diff(u, 2) - 0.0001*diff(u, 4) - 0.01*u;
+S.nonlin = @(u) 0.1*diff(u.^3, 2);
 % S.nonlin = @(u) 0.75*u.^2.*diff(u,2) + 1.5*u.*diff(u,1).^2;
 S.init = u0;
 u = spin(S, N, dt, 'plot', 'off');
